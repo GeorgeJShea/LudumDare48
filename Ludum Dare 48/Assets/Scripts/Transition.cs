f6ba6e8e5082f0aaa8800329eb28e9b503ac0005
Szilagyi Matyas <fastgamingyt.mc444@gmail.com>
2021-04-25 18:17:12 +0300

    Fixed Main Menu

--- Ludum Dare 48/Assets/Scripts/Transition.cs
+++ Ludum Dare 48/Assets/Scripts/Transition.cs
@@ -9,15 +9,13 @@ public class Transition : MonoBehaviour
 {
     public GameObject screenFade;
     public int nextLevel;
-    public bool died = false;
+
     public void Clicked()
     {
-        StartCoroutine(FadeImage(nextLevel, died));
+        StartCoroutine(FadeImage(nextLevel));
     }
-    public IEnumerator FadeImage(int nextSceneToLoad, bool died)
+    public IEnumerator FadeImage(int nextSceneToLoad)
     {
-        //Sets up scene
-        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex;
         // Grabs fade object infront of camera. 
         GameObject screenFade = GameObject.Find("ScreenFade");
 
@@ -25,28 +23,15 @@ public class Transition : MonoBehaviour
         image.enabled = true;
 
         // Grabs u died game object and sets it off
-        GameObject uDied = transform.GetChild(0).gameObject;
-        uDied.active = false;
-
         for (float i = 0; i <= 1; i += Time.deltaTime)
         {
             // set color with i as alpha
             image.color = new Color(0, 0, 0, i);
             yield return null;
         }
-        //Set to true if you are respawning and to current level for respawn
-        if(died == true)
-        {
-            uDied.active = true;
-            yield return new WaitForSeconds(3);
-
-            Debug.Log("you ded");
-            SceneManager.LoadScene(nextSceneToLoad);
-        }
 
         Debug.Log("next scene tottaly loaded got o script when we actually have scenes");
-        
-        
+       
         SceneManager.LoadScene(nextSceneToLoad);
     }
 }
