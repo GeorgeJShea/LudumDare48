9cb2eb08457920a92d4f8c43ac1154fe769d0661
Szilagyi Matyas <fastgamingyt.mc444@gmail.com>
2021-04-27 00:30:59 +0300

    Added barrel explosion

--- Ludum Dare 48/Assets/Scripts/Destructable.cs
+++ Ludum Dare 48/Assets/Scripts/Destructable.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class Destructable : MonoBehaviour, IHitable
 {
-    public SpriteRenderer rend;
-    public Sprite[] Sprites;
-
     public GameObject[] ToSpawn;
 
     public float Health { get { return 1; } set { } }
-
-    private void Awake()
-    {
-        rend.sprite = Sprites[Random.Range(0, Sprites.Length)];
-    }
 
     public void Damage(float damage, bool makeHitSound = true)
     {
         if (ToSpawn.Length > 0) Instantiate(ToSpawn[Random.Range(0, ToSpawn.Length)], transform.position + (Vector3.up * 0.5f), Quaternion.identity);
 
-        Destroy(gameObject);
+        GetComponent<Animator>().Play("BarrelExplode");
+
+        Destroy(GetComponent<BoxCollider2D>());
+        Destroy(this);
     }
 
     public virtual void Hit(Projectile projectile, out bool destroy)
