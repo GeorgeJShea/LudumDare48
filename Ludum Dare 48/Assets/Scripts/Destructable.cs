13d692d117d8df6425b6ee66c36c92bbca0646be
Szilagyi Matyas <fastgamingyt.mc444@gmail.com>
2021-04-26 22:19:55 +0300

    Added destructible

--- Ludum Dare 48/Assets/Scripts/Destructable.cs
+++ Ludum Dare 48/Assets/Scripts/Destructable.cs
@@ -7,16 +7,25 @@ public class Destructable : MonoBehaviour, IHitable
     public SpriteRenderer rend;
     public Sprite[] Sprites;
 
-    public float Health { get { return health; } set { health = value; } }
-    private float health;
+    public float Health { get { return 1; } set { } }
+
+    private void Awake()
+    {
+        rend.sprite = Sprites[Random.Range(0, Sprites.Length)];
+    }
 
     public void Damage(float damage, bool makeHitSound = true)
     {
         Destroy(gameObject);
     }
 
-    public void Hit(Projectile projectile, out bool destroy)
+    public virtual void Hit(Projectile projectile, out bool destroy)
     {
-        throw new System.NotImplementedException();
+        projectile.gameObject.transform.parent = null;
+        //Instantiate(sucHit, projectile.transform.position, Quaternion.identity);
+
+        Damage(projectile.damage);
+
+        destroy = true;
     }
 }
