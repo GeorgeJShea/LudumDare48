88f46ffa2ba8641efa65d0f03a59d120b98108c9
Szilagyi Matyas <fastgamingyt.mc444@gmail.com>
2021-04-26 21:57:21 +0300

    .

--- Ludum Dare 48/Assets/Scripts/Destructable.cs
+++ Ludum Dare 48/Assets/Scripts/Destructable.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destructable : MonoBehaviour
+public class Destructable : MonoBehaviour, IHitable
 {
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRenderer rend;
+    public Sprite[] Sprites;
+
+    public float Health { get { return health; } set { health = value; } }
+    private float health;
+
+    public void Damage(float damage, bool makeHitSound = true)
     {
-        
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Hit(Projectile projectile, out bool destroy)
     {
-        
+        throw new System.NotImplementedException();
     }
 }
